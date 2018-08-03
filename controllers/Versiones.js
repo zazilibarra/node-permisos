127288a408cb053095d51e143271d756dcaf10e5
imorales-logistikgo <imorales@logistikgo.com>
2018-08-03 09:23:15 -0500

    ..

--- controllers/Versiones.js
+++ controllers/Versiones.js
@@ -20,26 +20,9 @@ function getVersiones(req,res) {
 
 function getUltimaVersion(req,res) {
 
-	versiones.find({}).sort({fechaActualizacion:-1}).exec(function(err,version){
-		if(err){
-			return res.status(500).send({message:"Error al realizar la peticion"});
-		}
+	let _entorno = req.params.entorno;
 
-		if(!version && version.length>0){
-			return res.status(500).send({message:"No existen datos"});
-		}	
-
-		res.status(200).send(version[0]);
-	});
-
-}
-
-function getUsuarioxVersion(req,res) {
-
-	let _idusuario = req.query.idusuario;
-	let _version = req.query.version;
-
-	versiones.find( { }, (err,version)=>{
+	versiones.find({entorno:_entorno}).sort({fechaActualizacion:-1}).exec(function(err,version){
 		if(err){
 			return res.status(500).send({message:"Error al realizar la peticion"});
 		}
@@ -56,5 +39,4 @@ function getUsuarioxVersion(req,res) {
 module.exports = {
 	getVersiones,
 	getUltimaVersion,
-	getUsuarioxVersion
 }
