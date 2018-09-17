4186a610f3a720206a9dd7d90435ca07d0d8b7ea
imorales-logistikgo <imorales@logistikgo.com>
2018-09-17 13:00:52 -0500

    add connection mlab

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	port:process.env.PORT || 8080,
-	db: process.env.MONGODB || 'mongodb://40.124.41.46:27017/LogistikGO_XD'
+	// db: process.env.MONGODB || 'mongodb://40.124.41.46:27017/LogistikGO_XD',
+	db: process.env.MONGODB || 'mongodb://admin:R9VdEsuAXRY3@ds131902.mlab.com:31902/logistikgo_xd_demo',
+	// db: process.env.MONGODB || `mongodb://logistikgo:7qcviPCAzXqhWYg4eaTwKlD5gme0oVavey3QjXZtx75XOc5t8ODNHkPbgFL6tIqIZFZRIvUNNW9fgxSDlDyByg%3D%3D@logistikgo.documents.azure.com:10255/logistikgo_xd?ssl=true&replicaSet=globaldb`,	
 }
